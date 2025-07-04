721245bce2a2f297bdb8a37cdb7aedc3dd80b12a
Lord Savith <bulletproofchild@gmail.com>
2025-07-03 18:00:58 -0600

    Setup basic Home Page Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,72 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import HeroImage from "/images/Hero-Image-Mobile.jpg";
+
+function Hero() {
+  return (
+    <section>
+      <img src={HeroImage} alt="Hero image placeholder" />
+      <h1>Caleb Mckays</h1>
+    </section>
+  );
+}
+
+function About() {
+  return (
+    <section>
+      <h2>About</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab quis
+        reiciendis impedit at quod aliquam. Doloribus nemo enim quo sunt
+        voluptatem iste praesentium beatae, modi cupiditate at, officiis,
+        recusandae esse.
+      </p>
+      <img src={HeroImage} alt="About image placeholder" />
+    </section>
+  );
+}
+
+function Projects() {
+  return (
+    <section>
+      <h2>Projects</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab quis
+        reiciendis impedit at quod aliquam. Doloribus nemo enim quo sunt
+        voluptatem iste praesentium beatae, modi cupiditate at, officiis,
+        recusandae esse.
+      </p>
+
+      <div>
+        <h3>Personal</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta at ad
+          possimus soluta velit voluptatum commodi id. In aspernatur
+          repudiandae, fuga velit obcaecati laboriosam culpa dolorem nobis
+          molestiae itaque dignissimos?
+        </p>
+        <img src={HeroImage} alt="About image placeholder" />
+      </div>
+
+      <div>
+        <h3>Professional</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta at ad
+          possimus soluta velit voluptatum commodi id. In aspernatur
+          repudiandae, fuga velit obcaecati laboriosam culpa dolorem nobis
+          molestiae itaque dignissimos?
+        </p>
+        <img src={HeroImage} alt="About image placeholder" />
+      </div>
+    </section>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Hero />
+      <About />
+      <Projects />
     </>
   );
 }
