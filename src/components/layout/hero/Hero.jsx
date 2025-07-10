eff5e4da299be313ad6eaaa897dd8a4d7c303396
Lord Savith <bulletproofchild@gmail.com>
2025-07-09 20:36:07 -0600

    add: animate hero code text

--- src/components/layout/hero/Hero.jsx
+++ src/components/layout/hero/Hero.jsx
@@ -8,8 +8,8 @@ function Hero() {
         <h1>Caleb Anderson</h1>
         <p>A Full Stack Developer & Engineer</p>
         <p className="hero-code">
-          <code>{"</>"}</code>
-          <code>{"{}"}</code>
+          <code className="code-html">{"</>"}</code>
+          <code className="code-object">{"{}"}</code>
         </p>
       </div>
       <ParticleCanvas />
