74a2764dde3fbbaee99d5fe8f390114c5cece002
Lord Savith <bulletproofchild@gmail.com>
2025-07-11 12:05:45 -0600

    Add: finish tabs; lift up toggle state to parent

--- src/components/common/tabs/Tabs.jsx
+++ src/components/common/tabs/Tabs.jsx
@@ -1,9 +1,47 @@
-function Tabs({ data }) {
+import "./Tabs.css";
+
+function Tabs({ data, activeId, toggleActive, windowSize }) {
+  // if there is no active ID then default to first item
+  !activeId && windowSize.width >= 768
+    ? toggleActive(0)
+    : toggleActive(activeId);
+
   return (
     <div className="tabs-wrapper">
       <div className="tabs">
-        <div className="tabs-titles"></div>
-        <div className="tabs-content"></div>
+        <div className="tabs-titles">
+          {data.map((tab) => (
+            <div
+              key={tab.id}
+              className={`tab-button ${tab.id === activeId ? "active" : ""}`}
+            >
+              <button
+                className="accordion-button"
+                onClick={() => toggleActive(tab.id)}
+              >
+                <h3>{tab.job}</h3>
+              </button>
+            </div>
+          ))}
+        </div>
+        <div className="tabs-content">
+          {data.map((job) => (
+            <div
+              className={`tab-content job ${
+                job.id === activeId ? "active" : ""
+              }`}
+            >
+              <p>{job.duration}</p>
+              <p>{job.title}</p>
+              <p>{job.content.description}</p>
+              <ul>
+                {job.content.duties.map((duty) => (
+                  <li key={duty}>{duty}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/layout/experience/Experience.jsx
+++ src/components/layout/experience/Experience.jsx
@@ -1,19 +1,54 @@
+import { useEffect, useState } from "react";
 import Accordion from "../../common/accordion/Accordion";
 import Tabs from "../../common/tabs/Tabs";
 import experienceData from "./data/experience";
 import "./Experience.css";
 
 function Experience() {
+  const [activeId, setActiveId] = useState(null);
+
+  const toggleActive = function (itemId) {
+    setActiveId(itemId);
+  };
+
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [windowSize]);
+
   return (
     <section id="experience" className="experience-section">
       <div className="experience-title">
         <h2>Experience</h2>
       </div>
 
-      {window.innerWidth < 768 ? (
-        <Accordion data={experienceData} />
+      {windowSize.width < 768 ? (
+        <Accordion
+          data={experienceData}
+          activeId={activeId}
+          toggleActive={toggleActive}
+        />
       ) : (
-        <Tabs data={experienceData} />
+        <Tabs
+          data={experienceData}
+          activeId={activeId}
+          toggleActive={toggleActive}
+          windowSize={windowSize}
+        />
       )}
     </section>
   );
