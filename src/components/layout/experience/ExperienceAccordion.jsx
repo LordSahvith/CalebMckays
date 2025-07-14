7233be8a84017650ada3ed54ef66bc9777ae2456
LordSahvith <bulletproofchild@gmail.com>
2025-07-13 22:19:31 -0600

    Update: Reformat Experience Tabs & Accordions

--- src/components/layout/experience/ExperienceAccordion.jsx
+++ src/components/layout/experience/ExperienceAccordion.jsx
@@ -5,6 +5,39 @@ import {
   AccordionContent,
 } from "../../common/accordion/Accordion";
 
+function Title({ item, activeId, action }) {
+  return (
+    <AccordionTitle item={item.id} action={action}>
+      <h3>{item.job}</h3>
+      <span className={`plus-icon ${activeId === item.id ? "active" : ""}`}>
+        <span className="bar bar-vertical"></span>
+        <span className="bar bar-horizontal"></span>
+      </span>
+    </AccordionTitle>
+  );
+}
+
+function Content({ item, activeId }) {
+  return (
+    <AccordionContent isActive={activeId === item.id}>
+      <div className="job">
+        <h4 className="job-title">{item.title}</h4>
+        <p>{item.duration}</p>
+        <div className="accordion-description">
+          {item.content.description.map((desc) => (
+            <p key={desc}>{desc}</p>
+          ))}
+        </div>
+        <ul>
+          {item.content.duties.map((duty) => (
+            <li key={duty}>{duty}</li>
+          ))}
+        </ul>
+      </div>
+    </AccordionContent>
+  );
+}
+
 function ExperienceAccordion({ data, activeId, setActiveId }) {
   const toggle = function (itemId) {
     return activeId === itemId ? setActiveId(null) : setActiveId(itemId);
@@ -14,31 +47,8 @@ function ExperienceAccordion({ data, activeId, setActiveId }) {
     <Accordion>
       {data.map((item) => (
         <AccordionItem key={item.id}>
-          <AccordionTitle item={item.id} action={toggle}>
-            <h3>{item.job}</h3>
-            <span
-              className={`plus-icon ${activeId === item.id ? "active" : ""}`}
-            >
-              <span className="bar bar-vertical"></span>
-              <span className="bar bar-horizontal"></span>
-            </span>
-          </AccordionTitle>
-          <AccordionContent isActive={activeId === item.id}>
-            <div className="job">
-              <h4 className="job-title">{item.title}</h4>
-              <p>{item.duration}</p>
-              <div className="accordion-description">
-                {item.content.description.map((desc) => (
-                  <p key={desc}>{desc}</p>
-                ))}
-              </div>
-              <ul>
-                {item.content.duties.map((duty) => (
-                  <li key={duty}>{duty}</li>
-                ))}
-              </ul>
-            </div>
-          </AccordionContent>
+          <Title item={item} activeId={activeId} action={toggle} />
+          <Content item={item} activeId={activeId} />
         </AccordionItem>
       ))}
     </Accordion>
--- src/components/layout/experience/ExperienceTabs.jsx
+++ src/components/layout/experience/ExperienceTabs.jsx
@@ -7,6 +7,40 @@ import {
   TabContent,
 } from "../../common/tabs/Tabs";
 
+function TabTitles({ data, activeId, action }) {
+  return data.map((tab) => (
+    <TabTitle
+      key={tab.id}
+      item={tab.id}
+      isActive={tab.id === activeId}
+      action={action}
+    >
+      <h3>{tab.job}</h3>
+    </TabTitle>
+  ));
+}
+
+function TabContents({ data, activeId }) {
+  return data.map((item) => (
+    <TabContent key={item.id} isActive={item.id === activeId}>
+      <div className="job">
+        <h4 className="job-title">{item.title}</h4>
+        <p>{item.duration}</p>
+        <div className="tab-description">
+          {item.content.description.map((desc) => (
+            <p key={desc}>{desc}</p>
+          ))}
+        </div>
+        <ul>
+          {item.content.duties.map((duty) => (
+            <li key={duty}>{duty}</li>
+          ))}
+        </ul>
+      </div>
+    </TabContent>
+  ));
+}
+
 function ExperienceTabs({ data, activeId, setActiveId }) {
   const toggle = function (itemId) {
     setActiveId(itemId);
@@ -22,36 +56,10 @@ function ExperienceTabs({ data, activeId, setActiveId }) {
   return (
     <Tabs>
       <TabsTitlesWrapper>
-        {data.map((tab) => (
-          <TabTitle
-            key={tab.id}
-            item={tab.id}
-            isActive={tab.id === activeId}
-            action={toggle}
-          >
-            <h3>{tab.job}</h3>
-          </TabTitle>
-        ))}
+        <TabTitles data={data} activeId={activeId} action={toggle} />
       </TabsTitlesWrapper>
       <TabsContentWrapper>
-        {data.map((item) => (
-          <TabContent key={item.id} isActive={item.id === activeId}>
-            <div className="job">
-              <h4 className="job-title">{item.title}</h4>
-              <p>{item.duration}</p>
-              <div className="tab-description">
-                {item.content.description.map((desc) => (
-                  <p key={desc}>{desc}</p>
-                ))}
-              </div>
-              <ul>
-                {item.content.duties.map((duty) => (
-                  <li key={duty}>{duty}</li>
-                ))}
-              </ul>
-            </div>
-          </TabContent>
-        ))}
+        <TabContents data={data} activeId={activeId} />
       </TabsContentWrapper>
     </Tabs>
   );
